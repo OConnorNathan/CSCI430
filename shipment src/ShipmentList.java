f51f9373ff6b9bf1995b1d5e4ab641be378ee875
Blake <83431780+blakehoosline@users.noreply.github.com>
2022-09-28 22:43:37 -0500

    Create ShipmentList.java

---
+++ shipment src/ShipmentList.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class ShipmentList {
+
+    private LinkedList<Shipment> shipments;
+
+    public ShipmentList() {
+        shipments = new LinkedList<Shipment>();
+    }
+
+    public boolean addShipment(Shipment shipment) {
+        return shipments.add(shipment);
+    }
+
+    public boolean receiveShipment(Shipment shipment) {
+        return shipments.add(shipment);
+    }
+
+    public boolean makePayment(Shipment shipment) {
+        return shipments.add(shipment);
+    }
+
+    public Iterator getShipments() {
+        return shipments.iterator();
+    }
+}
