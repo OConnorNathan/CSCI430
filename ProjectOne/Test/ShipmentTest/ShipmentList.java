b937f33c98471094f8b207c602905270ec943ad7
Blake <83431780+blakehoosline@users.noreply.github.com>
2022-10-01 02:51:34 -0500

    Create ShipmentList.java

---
+++ ProjectOne/Test/ShipmentTest/ShipmentList.java
@@ -0,0 +1,75 @@
+/*******************************************************************
+ * 
+ * Project 1: Warehouse, ShipmentHistory implementation
+ * File: ShipmentHistory.java
+ * 
+ * Author: Blake Hoosline
+ * Group Number: 2
+ * Instructor: Dr. Ramnath Sarnath
+ * Class: CSCI 430
+ * 
+ * Based On: Catalog.java by Dr. Ramnath Sarnath
+ * 
+ *******************************************************************/
+//package ProjectOne.src;
+
+import java.util.*;
+import java.io.*;
+
+public class ShipmentList implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private LinkedList<Shipment> shipments = new LinkedList<Shipment>();
+    private static ShipmentList shipmentList;
+
+    private ShipmentList() {
+    }
+
+    public static ShipmentList instance() {
+        if (shipmentList == null) {
+            return (shipmentList = new ShipmentList());
+        } else {
+            return shipmentList;
+        }
+    }
+
+    public boolean insertShipment(Shipment shipment) {
+        return shipments.add(shipment);
+    }
+
+    public Iterator getShipments() {
+        return shipments.iterator();
+    }
+
+    private void writeObject(java.io.ObjectOutputStream output) {
+        try {
+            output.defaultWriteObject();
+            output.writeObject(shipmentList);
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+    }
+
+    private void readObject(java.io.ObjectInputStream input) {
+        try {
+            if (shipmentList != null) {
+                return;
+            } else {
+                input.defaultReadObject();
+                if (shipmentList == null) {
+                    shipmentList = (ShipmentList) input.readObject();
+                } else {
+                    input.readObject();
+                }
+            }
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        } catch (ClassNotFoundException cnfe) {
+            cnfe.printStackTrace();
+        }
+    }
+
+    public String toString() {
+        return shipments.toString();
+    }
+
+}
